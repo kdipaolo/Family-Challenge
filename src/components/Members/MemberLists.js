a4f8b09959ae4ce5c3570afc2216599aa450386f
Kurt DiPaolo <kurtdipaolo@gmail.com>
2017-12-11 09:48:39 -0500

    ability to remove member from family (#33)

--- src/components/Members/MemberLists.js
+++ src/components/Members/MemberLists.js
@@ -1,16 +1,34 @@
-import React from "react"
-import Task from "../task/TaskCard"
-import ContentWrapper from "../../styles/ContentWrapper"
-import Switcher from "../shared/Switcher"
-
+import React from 'react'
+import { gql, graphql, withApollo, compose } from 'react-apollo'
+import Task from '../task/TaskCard'
+import ContentWrapper from '../../styles/ContentWrapper'
+import Switcher from '../shared/Switcher'
+import Button from '../shared/Button'
+import { USER_ID } from '../../utils/constants'
+import { withRouter } from 'react-router-dom'
 class MemberLists extends React.Component {
   state = {
-    active: "Tasks"
+    active: 'Tasks'
   }
   handleSwitcherClick = e => {
     this.setState({
       active: e.target.dataset.item
     })
+  }
+  handleRemoveMember = async () => {
+    const membersIds = await this.props.getMembers.User.members.map(
+      member => member.id
+    )
+    const index = membersIds.indexOf(this.props.match.params.memberid)
+    membersIds.splice(index, 1)
+
+    await this.props.removeMemberFromFamily({
+      variables: {
+        id: localStorage.getItem(USER_ID),
+        membersIds
+      }
+    })
+    this.props.history.push('/members')
   }
   render() {
     return (
@@ -18,10 +36,10 @@ class MemberLists extends React.Component {
         <Switcher
           handleSwitcherClick={this.handleSwitcherClick}
           active={this.state.active}
-          links={["Tasks", "Settings"]}
+          links={['Tasks', 'Settings']}
         />
         <ContentWrapper>
-          {this.state.active === "Tasks" ? (
+          {this.state.active === 'Tasks' ? (
             <div>
               {this.props.User.tasks.map(task => (
                 <Task key={task.id} {...task} />
@@ -30,6 +48,9 @@ class MemberLists extends React.Component {
           ) : (
             <div>
               <h1>Settings</h1>
+              <Button danger onClick={this.handleRemoveMember}>
+                Remove member from family
+              </Button>
             </div>
           )}
         </ContentWrapper>
@@ -38,4 +59,32 @@ class MemberLists extends React.Component {
   }
 }
 
-export default MemberLists
+const UPDATE_MEMBERS_MUTATION = gql`
+  mutation removeMemberFromFamily($id: ID!, $membersIds: [ID!]) {
+    updateUser(id: $id, membersIds: $membersIds) {
+      id
+    }
+  }
+`
+
+const GET_MEMBERS_IN_FAMILY = gql`
+  query getMembersInFamily($id: ID!) {
+    User(id: $id) {
+      members {
+        id
+      }
+    }
+  }
+`
+
+export default withRouter(
+  compose(
+    graphql(UPDATE_MEMBERS_MUTATION, {
+      name: 'removeMemberFromFamily'
+    }),
+    graphql(GET_MEMBERS_IN_FAMILY, {
+      name: 'getMembers',
+      options: props => ({ variables: { id: localStorage.getItem(USER_ID) } })
+    })
+  )(MemberLists)
+)
